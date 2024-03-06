c3cdbacffe35b598757dc62a6a312efe455322f2
Ajil Kumar A <72295862+ajilkumar@users.noreply.github.com>
2024-03-06 19:48:16 +0530

    Add files via upload

---
+++ leapyear.py
@@ -0,0 +1,23 @@
+print("Welcome to pizza delivery!!")
+size = input("What size pizza do you want? L, M or S")
+pepperoni = input ("Do you want pepperoni? Y or N")
+cheese = input("Do you want extra cheese? Y or N")
+
+bill = 0
+
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill +=25
+
+if pepperoni == "Y":
+    if size == "S":
+        bill +=2
+    else:
+        bill +=3
+if cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is {bill}")
